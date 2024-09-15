1fae65d5606b103925d64f7d0a0216727e64694d
Kuang-Yang <56757171+aurorasun27@users.noreply.github.com>
2024-09-14 23:56:46 -0400

    Modify 02 codes.

--- 02-image-denoising/image_denoising.py
+++ 02-image-denoising/image_denoising.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def get_noisy_image(path, mean=0, sigma=5):
+def get_noisy_image(path, mean, sigma):
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    #img = cv2.resize(img, (1000, 750))
+    img = cv2.resize(img, (2000, 1500))
     gauss_noise = np.random.normal(mean, sigma, img.shape).astype(img.dtype)
     img_noisy = cv2.add(img, gauss_noise)
     return img, img_noisy
 
-def get_noisy_image_color(path, mean=0, sigma=5):
+def get_noisy_image_color(path, mean, sigma):
     img= cv2.imread(path) #BGR
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #img = cv2.resize(img, (1000, 750))
+    img = cv2.resize(img, (2000, 1500))
     row, col, ch = img.shape
     gauss = np.random.normal(mean, sigma, (row, col, ch))
     gauss_noise = gauss.reshape(row, col, ch).astype('uint8')
@@ -44,46 +44,50 @@ def visualization(imgs: dict, save_name):
     plt.show()
 
 def get_mean_filter_results(img_orig, img_noisy):
-    mean_filtering_3 = cv2.blur(img_noisy, (3,3))
-    mean_filtering_9 = cv2.blur(img_noisy, (9,9))
-    mean_filtering_15 = cv2.blur(img_noisy, (15,15))
-    mean_filtering_21 = cv2.blur(img_noisy, (21,21))
+    mean_filtering_3 = cv2.blur(img_noisy, (3, 3))
+    mean_filtering_9 = cv2.blur(img_noisy, (9, 9))
+    mean_filtering_21 = cv2.blur(img_noisy, (21, 21))
+    mean_filtering_39 = cv2.blur(img_noisy, (39, 39))
 
-    imgs = {'img_orig': img_orig, 'img_noisy': img_noisy, 'mean_filter_3': mean_filtering_3,
-            'mean_filter_9': mean_filtering_9, 'mean_filter_15': mean_filtering_15, 'mean_filter_21': mean_filtering_21}
+    imgs = {'img_orig': img_orig,
+            'img_noisy': img_noisy,
+            'mean_filter_3': mean_filtering_3,
+            'mean_filter_9': mean_filtering_9,
+            'mean_filter_21': mean_filtering_21,
+            'mean_filter_39': mean_filtering_39 }
     return imgs
 
 def get_gaussian_filter_by_size_results(img_orig, img_noisy):
-    gaussian_filtering_3 = cv2.GaussianBlur(img_noisy, (3,3), 0)
-    gaussian_filtering_9 = cv2.GaussianBlur(img_noisy, (9,9), 0)
-    gaussian_filtering_15 = cv2.GaussianBlur(img_noisy, (15,15), 0)
-    gaussian_filtering_21 = cv2.GaussianBlur(img_noisy, (21,21), 0)
+    gaussian_filtering_3 = cv2.GaussianBlur(img_noisy, (3, 3), 0)
+    gaussian_filtering_9 = cv2.GaussianBlur(img_noisy, (9, 9), 0)
+    gaussian_filtering_21 = cv2.GaussianBlur(img_noisy, (21, 21), 0)
+    gaussian_filtering_39 = cv2.GaussianBlur(img_noisy, (39, 39), 0)
 
     imgs = {'img_orig': img_orig,
             'img_noisy': img_noisy,
             'gaussian_filter_3': gaussian_filtering_3,
             'gaussian_filter_9': gaussian_filtering_9,
-            'gaussian_filter_15': gaussian_filtering_15,
-            'gaussian_filter_21': gaussian_filtering_21}
+            'gaussian_filter_21': gaussian_filtering_21,
+            'gaussian_filter_39': gaussian_filtering_39}
     return imgs
 
 def get_median_filter_results(img_orig, img_noisy):
     median_filtering_3 = cv2.medianBlur(img_noisy, 3)
     median_filtering_9 = cv2.medianBlur(img_noisy, 9)
-    median_filtering_15 = cv2.medianBlur(img_noisy, 15)
     median_filtering_21 = cv2.medianBlur(img_noisy, 21)
+    median_filtering_39 = cv2.medianBlur(img_noisy, 39)
     imgs = {'img_orig': img_orig,
             'img_noisy': img_noisy,
             'median_filter_3': median_filtering_3,
             'median_filter_9': median_filtering_9,
-            'median_filter_15': median_filtering_15,
-            'median_filter_21': median_filtering_21}
+            'median_filter_21': median_filtering_21,
+            'median_filter_39': median_filtering_39}
     return imgs
 
 if __name__ == '__main__':
 
     # original image visualization
-    img_color_orig, img_color_noisy = get_noisy_image_color(sys.argv[1], mean=0, sigma=2)
+    img_color_orig, img_color_noisy = get_noisy_image_color(sys.argv[1], 0, 1)
     plt.subplot(1,2,1); plt.title('original image'); plt.axis('off'); plt.imshow(img_color_orig)
     plt.subplot(1,2,2); plt.title('noisy image'); plt.axis('off'); plt.imshow(img_color_noisy)
     fig = plt.gcf()
